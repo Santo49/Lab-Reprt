c910479f1aa752768dcc30ab16a6f50fd74cb5ca
Santo49 <109537666+Santo49@users.noreply.github.com>
2022-08-15 19:36:44 +0600

    Update 2. 352-49-8.cpp

--- lab 10/2. 352-49-8.cpp
+++ lab 10/2. 352-49-8.cpp
@@ -1,1 +1,17 @@
+//CodeForces Problem Name: Watermelon
+//Problem Code: 4A
 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int w;
+    cin>>w;
+    if(w % 2 == 0){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+    return 0;
+}
